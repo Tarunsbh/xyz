d5d0ae8ccdf1d05d543a269393e87cb23b67b4c5
Tarun Sabharwal <tarunsbh@gmail.com>
2022-08-23 23:50:14 +0530

    first commit

---
+++ funtion.js
@@ -0,0 +1,21 @@
+// function hello(){
+//     console.log("hello world");
+// }
+// hello();
+
+function rectangleArea(x,y){
+    console.log("Area of triangle is: ", x*y );
+}
+
+function squareArea(x){
+    console.log("Area of square is: ", x*x);
+}
+
+function circleArea(r){
+
+    console.log("Area of circle is: ", 3.14*r*r);
+}
+
+rectangleArea(5,10)
+squareArea(5)
+circleArea(5)
